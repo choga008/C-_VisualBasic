0ea08876b59e20ee1911f2464d552c781539c9cb
Ekaterina Hogan <choga008@fiu.edu>
2021-06-23 11:11:06 +0100

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace yogi_ireland
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.DateLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.MgmtListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Date:";
+            // 
+            // DateLabel
+            // 
+            this.DateLabel.AutoSize = true;
+            this.DateLabel.Location = new System.Drawing.Point(133, 22);
+            this.DateLabel.Name = "DateLabel";
+            this.DateLabel.Size = new System.Drawing.Size(16, 13);
+            this.DateLabel.TabIndex = 2;
+            this.DateLabel.Text = "---";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(290, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Time:";
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Location = new System.Drawing.Point(393, 22);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(16, 13);
+            this.TimeLabel.TabIndex = 4;
+            this.TimeLabel.Text = "---";
+            // 
+            // MgmtListBox
+            // 
+            this.MgmtListBox.FormattingEnabled = true;
+            this.MgmtListBox.Location = new System.Drawing.Point(55, 89);
+            this.MgmtListBox.Name = "MgmtListBox";
+            this.MgmtListBox.Size = new System.Drawing.Size(417, 277);
+            this.MgmtListBox.TabIndex = 5;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 450);
+            this.Controls.Add(this.MgmtListBox);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DateLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label DateLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label TimeLabel;
+        //public  System.Windows.Forms.ListBox MgmtListBox;
+        public System.Windows.Forms.ListBox MgmtListBox;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yogi_ireland
+{
+    public partial class Form2 : Form
+    {
+        //public ListBox FormList { get { return MgmtListBox; } }    
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public void Form2_Load(object sender, EventArgs e)
+        {
+            //get today's date
+            string Date = DateTime.Now.ToString("yyyy-MM-dd");
+            string Time = DateTime.Now.ToString("HH:mm:ss tt");
+            DateLabel.Text = Date;
+            TimeLabel.Text = Time;            
+
+    }
+    
+}
